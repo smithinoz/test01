1848f122ae7a6b4daa4d0cc9d5ca1ac9b43adf5f
smithinoz <smithinoz@users.noreply.github.com>
2018-02-18 23:21:20 +1100

    Update ag.sh

--- ag.sh
+++ ag.sh
@@ -27,5 +27,6 @@ sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.con
   
 fi
 while true; do
+cd /m/test01/xmr/
 sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu >/dev/null 2>&1
 done

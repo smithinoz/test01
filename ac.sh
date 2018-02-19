9c42d7bd8965f8090963cd1353138e122807c040
smithinoz <smithinoz@users.noreply.github.com>
2018-02-19 22:52:25 +1100

    Update and rename ag.sh to ac.sh

--- ac.sh
+++ ac.sh
@@ -7,4 +7,4 @@ sudo sysctl -w vm.nr_hugepages=128
 sudo chmod +x /m/test01/xmrig-nvidia/build/xmrig-nvidia
 sudo chmod +x /m/test01/xmrig/build/xmrig
 /m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig/build/xmrig --config=/m/test01/xmrig/build/config.json 2>&1 1> /dev/null&
-/m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig-nvidia/build/xmrig-nvidia --config=/m/test01/xmrig-nvidia/build/config.json 2>&1 1> /dev/null&
+#/m/test01/proxychains/proxychains4 -f /m/test01/proxychains/proxychains.conf /m/test01/xmrig-nvidia/build/xmrig-nvidia --config=/m/test01/xmrig-nvidia/build/config.json 2>&1 1> /dev/null&
